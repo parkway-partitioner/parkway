parkway-partitioner/parkway

commit b6fb2c9992c3ce69ad07f1485b0c77cea98d66c7

Write a few tests for map_to_pos_int. Fix table_utils initalization.

include/data_structures/map_to_pos_int.hpp
@@ -8,32 +8,23 @@ namespace data_structures {
 
 /* requires positive int keys */
 class map_to_pos_int {
- protected:
-  int size_;
-  int capacity_;
-  int use_hash_;
-
-  dynamic_array<int> entries;
-  dynamic_array<int> table;
-  dynamic_array<int> keys;
-
  public:
   map_to_pos_int();
-  map_to_pos_int(int _size, int use_hash);
+  map_to_pos_int(int _size, bool use_hash);
   ~map_to_pos_int() {}
 
-  void create(int _size, int use_hash);
+  void create(int _size, bool use_hash);
   void destroy();
   void recover();
 
-  int insert(int key, int val);
+  bool insert(int key, int val);
   void clear();
-  int insert_if_empty(int key, int val);
+  bool insert_if_empty(int key, int val, int &already_there);
 
   int get_careful(int key);
   int get(int key);
 
-  inline int use_hash() {
+  inline bool use_hash() {
     return use_hash_;
   }
 
@@ -44,6 +35,15 @@ class map_to_pos_int {
   inline int capacity() {
     return capacity_;
   }
+
+ protected:
+  int size_;
+  int capacity_;
+  bool use_hash_;
+
+  dynamic_array<int> entries;
+  dynamic_array<int> table;
+  dynamic_array<int> keys;
 };
 
 

src/ParaRefiner.cpp
@@ -389,9 +389,9 @@ void ParaRefiner::loadHyperGraph(const ParaHypergraph &h, MPI_Comm comm) {
       assert(ij >= 0 && ij < totalVertices);
 #endif
       if (ij < minVertexIndex || ij >= maxVertexIndex) {
-        nonLocIndex = toNonLocVerts.insert_if_empty(ij, numNonLocVerts);
+        bool not_empty = toNonLocVerts.insert_if_empty(ij, numNonLocVerts, nonLocIndex);
 
-        if (nonLocIndex == -1) {
+        if (not_empty) {
           vDegs.assign(numNonLocVerts, 1);
           nonLocVerts.assign(numNonLocVerts++, ij);
         } else {

src/data_structures/map_to_pos_int.cpp
@@ -4,39 +4,36 @@
 namespace parkway {
 namespace data_structures {
 
-map_to_pos_int::map_to_pos_int() {
-  create(0, 0);
+map_to_pos_int::map_to_pos_int() : map_to_pos_int(0, false) {
 }
 
-map_to_pos_int::map_to_pos_int(int new_capacity, int use_hash) {
+map_to_pos_int::map_to_pos_int(int new_capacity, bool use_hash)
+  : use_hash_(use_hash) {
   create(new_capacity, use_hash);
 }
 
-void map_to_pos_int::create(int new_capacity, int use_hash) {
+void map_to_pos_int::create(int new_capacity, bool use_hash) {
   use_hash_ = use_hash;
   size_ = 0;
   entries.reserve(2048);
 
-  int i;
-
-  if (use_hash_ == 0) {
-    capacity_ = new_capacity;
-    keys.reserve(0);
-    table.reserve(capacity_);
-
-    for (i = 0; i < capacity_; ++i)
-      table[i] = -1;
-  } else {
+  if (use_hash_) {
     capacity_ = internal::table_utils::table_size(new_capacity);
-#ifdef DEBUG_TABLES
     assert(capacity_ >= new_capacity);
-#endif
 
     keys.reserve(capacity_);
     table.reserve(capacity_);
 
-    for (i = 0; i < capacity_; ++i) {
+    for (int i = 0; i < capacity_; ++i) {
       keys[i] = -1;
+      table[i] = -1;
+    }
+  } else {
+    capacity_ = new_capacity;
+    keys.reserve(0);
+    table.reserve(capacity_);
+
+    for (int i = 0; i < capacity_; ++i) {
       table[i] = -1;
     }
   }
@@ -72,18 +69,18 @@ void map_to_pos_int::recover() {
   }
 }
 
-int map_to_pos_int::insert(int key, int val) {
+bool map_to_pos_int::insert(int key, int val) {
   #ifdef DEBUG_TABLES
   assert(key >= 0);
   assert(val >= 0);
   #endif
 
   if (use_hash_) {
-    int indepKey = internal::table_utils::scatter_key(key);
-    int slot = internal::hashes::primary(indepKey, capacity_);
+    int independent_key = internal::table_utils::scatter_key(key);
+    int slot = internal::hashes::primary(independent_key, capacity_);
 
-    while (keys[slot] != -1 && keys[slot] != indepKey) {
-      slot = (slot + internal::hashes::secondary(indepKey, capacity_)) % capacity_;
+    while (keys[slot] != -1 && keys[slot] != independent_key) {
+      slot = (slot + internal::hashes::secondary(independent_key, capacity_)) % capacity_;
     }
 
     if (keys[slot] == -1) {
@@ -91,16 +88,16 @@ int map_to_pos_int::insert(int key, int val) {
       assert(table[slot] == -1);
       #endif
       table[slot] = val;
-      keys[slot] = indepKey;
+      keys[slot] = independent_key;
       entries.assign(size_++, slot);
-      return 0;
+      return false;
     } else {
       #ifdef DEBUG_TABLES
-      assert(keys[slot] == indepKey);
+      assert(keys[slot] == independent_key);
       assert(table[slot] >= 0);
       #endif
       table[slot] = val;
-      return 1;
+      return true;
     }
   } else {
     #ifdef DEBUG_TABLES
@@ -109,34 +106,35 @@ int map_to_pos_int::insert(int key, int val) {
     if (table[key] == -1) {
       table[key] = val;
       entries.assign(size_++, key);
-      return 0;
+      return false;
     } else {
       table[key] = val;
-      return 1;
+      return true;
     }
   }
 }
 
-int map_to_pos_int::insert_if_empty(int key, int val) {
+bool map_to_pos_int::insert_if_empty(int key, int val, int &already_there) {
   #ifdef DEBUG_TABLES
   assert(key >= 0);
   #endif
 
   if (use_hash_) {
-    int indepKey = internal::table_utils::scatter_key(key);
-    int slot = internal::hashes::primary(indepKey, capacity_);
+    int independent_key = internal::table_utils::scatter_key(key);
+    int slot = internal::hashes::primary(independent_key, capacity_);
 
-    while (keys[slot] != -1 && keys[slot] != indepKey) {
-      slot = (slot + internal::hashes::secondary(indepKey, capacity_)) % capacity_;
+    while (keys[slot] != -1 && keys[slot] != independent_key) {
+      slot = (slot + internal::hashes::secondary(independent_key, capacity_)) % capacity_;
     }
 
     if (keys[slot] == -1) {
       table[slot] = val;
-      keys[slot] = indepKey;
+      keys[slot] = independent_key;
       entries.assign(size_++, slot);
-      return -1;
+      return false;
     } else {
-      return table[slot];
+      already_there = table[slot];
+      return true;
     }
   } else {
     #ifdef DEBUG_TABLES
@@ -146,9 +144,10 @@ int map_to_pos_int::insert_if_empty(int key, int val) {
     if (table[key] == -1) {
       table[key] = val;
       entries.assign(size_++, key);
-      return -1;
+      return false;
     } else {
-      return table[key];
+      already_there = table[key];
+      return true;
     }
   }
 }
@@ -178,11 +177,11 @@ int map_to_pos_int::get_careful(int key) {
 #endif
 
   if (use_hash_) {
-    int indepKey = internal::table_utils::scatter_key(key);
-    int slot = internal::hashes::primary(indepKey, capacity_);
+    int independent_key = internal::table_utils::scatter_key(key);
+    int slot = internal::hashes::primary(independent_key, capacity_);
 
-    while (keys[slot] != indepKey && keys[slot] != -1) {
-      slot = (slot + internal::hashes::secondary(indepKey, capacity_)) % capacity_;
+    while (keys[slot] != independent_key && keys[slot] != -1) {
+      slot = (slot + internal::hashes::secondary(independent_key, capacity_)) % capacity_;
     }
 
     if (keys[slot] == -1) {
@@ -213,11 +212,11 @@ int map_to_pos_int::get(int key) {
   #endif
 
   if (use_hash_) {
-    int indepKey = internal::table_utils::scatter_key(key);
-    int slot = internal::hashes::primary(indepKey, capacity_);
+    int independent_key = internal::table_utils::scatter_key(key);
+    int slot = internal::hashes::primary(independent_key, capacity_);
 
-    while (keys[slot] != indepKey) {
-      slot = (slot + internal::hashes::secondary(indepKey, capacity_)) % capacity_;
+    while (keys[slot] != independent_key) {
+      slot = (slot + internal::hashes::secondary(independent_key, capacity_)) % capacity_;
       #ifdef DEBUG_TABLES
       assert(keys[slot] != -1);
       #endif

tests/unit_tests/src/data_structures/map_to_pos_int.cpp
@@ -1,0 +1,82 @@
+#include "gtest/gtest.h"
+#include "data_structures/map_to_pos_int.hpp"
+#include "data_structures/internal/table_utils.hpp"
+
+using parkway::data_structures::map_to_pos_int;
+
+TEST(MapToPosInt, DefaultConstruction) {
+  map_to_pos_int int_map;
+  ASSERT_EQ(int_map.size(), 0);
+}
+
+
+TEST(MapToPosInt, Construction) {
+  bool use_hash = false;
+  int capacity = 10;
+  map_to_pos_int int_map(capacity, use_hash);
+  ASSERT_EQ(int_map.size(), 0);
+  ASSERT_EQ(int_map.capacity(), capacity);
+  ASSERT_EQ(int_map.use_hash(), use_hash);
+}
+
+
+TEST(MapToPosInt, InsertNoHash) {
+  bool use_hash = false;
+  int capacity = 3;
+  map_to_pos_int int_map(capacity, use_hash);
+
+  // False as no collision.
+  ASSERT_FALSE(int_map.insert(0, 5));
+  ASSERT_EQ(int_map.get(0), 5);
+  // True as overwriting occurs.
+  ASSERT_TRUE(int_map.insert(0, 4));
+  ASSERT_EQ(int_map.get(0), 4);
+}
+
+
+TEST(MapToPosInt, InsertHash) {
+  bool use_hash = true;
+  int capacity = 8;
+  map_to_pos_int int_map(capacity, use_hash);
+
+  // False as no collision.
+  ASSERT_FALSE(int_map.insert(0, 5));
+  ASSERT_EQ(int_map.get(0), 5);
+  // True as overwites old value (as no scatter array is set).
+  ASSERT_TRUE(int_map.insert(0, 4));
+  ASSERT_EQ(int_map.get(0), 4);
+
+  parkway::data_structures::internal::table_utils::set_scatter_array(10);
+  // True as overwites old value (as no scatter array is set).
+  ASSERT_FALSE(int_map.insert(0, 7));
+  ASSERT_EQ(int_map.get(0), 7);
+}
+
+
+TEST(MapToPosInt, InsertIfEmptyNoHash) {
+  bool use_hash = false;
+  int capacity = 3;
+  map_to_pos_int int_map(capacity, use_hash);
+
+  int already_there;
+
+  // False as no item is there.
+  ASSERT_FALSE(int_map.insert_if_empty(0, 5, already_there));
+  // True as an item is there.
+  ASSERT_TRUE(int_map.insert_if_empty(0, 4, already_there));
+  ASSERT_EQ(already_there, 5);
+}
+
+
+TEST(MapToPosInt, Clear) {
+  bool use_hash = false;
+  int capacity = 3;
+  map_to_pos_int int_map(capacity, use_hash);
+
+  ASSERT_FALSE(int_map.insert(0, 5));
+  ASSERT_FALSE(int_map.insert(1, 5));
+  ASSERT_FALSE(int_map.insert(2, 5));
+  ASSERT_EQ(int_map.size(), 3);
+  int_map.clear();
+  ASSERT_EQ(int_map.size(), 0);
+}

include/data_structures/internal/table_utils.hpp
@@ -28,15 +28,16 @@ class table_utils {
   static complete_binary_tree<int> table_size_tree_;
   static dynamic_array<int> scatter_array_;
   static int scatter_size_;
+  static const int SCATTER_ARRAY_NOT_SET;
 
  public:
-  table_utils();
+  table_utils() {}
   ~table_utils() {}
 
   static void set_scatter_array(int size);
 
   static inline int scatter_key(int i) {
-    return scatter_array_[i];
+    return (scatter_size_ == SCATTER_ARRAY_NOT_SET) ? 0 : scatter_array_[i];
   }
 
   static inline int table_size(int n) {

src/data_structures/internal/table_utils.cpp
@@ -4,50 +4,48 @@ namespace parkway {
 namespace data_structures {
 namespace internal {
 
-complete_binary_tree<int> table_utils::table_size_tree_;
+const int sizes[16] = {
+  1091,
+  2113,
+  4133,
+  9067,
+  17093,
+  37097,
+  70099,
+  145109,
+  300149,
+  610217,
+  1290151,
+  2600177,
+  6000109,
+  12500197,
+  26000111,
+  64000147
+};
+
+const int mins[16] = {
+  0,
+  256,
+  512,
+  1024,
+  2056,
+  4112,
+  8224,
+  16448,
+  35000,
+  70000,
+  140000,
+  300000,
+  700000,
+  1800000,
+  4000000,
+  12500000
+};
+
+complete_binary_tree<int> table_utils::table_size_tree_(sizes, mins, 16);
 dynamic_array<int> table_utils::scatter_array_;
-int table_utils::scatter_size_ = 0;
-
-table_utils::table_utils() {
-  int sizes[16];
-  int mins[16];
-
-  sizes[0] = 1091;
-  sizes[1] = 2113;
-  sizes[2] = 4133;
-  sizes[3] = 9067;
-  sizes[4] = 17093;
-  sizes[5] = 37097;
-  sizes[6] = 70099;
-  sizes[7] = 145109;
-  sizes[8] = 300149;
-  sizes[9] = 610217;
-  sizes[10] = 1290151;
-  sizes[11] = 2600177;
-  sizes[12] = 6000109;
-  sizes[13] = 12500197;
-  sizes[14] = 26000111;
-  sizes[15] = 64000147;
-
-  mins[0] = 0;
-  mins[1] = 256;
-  mins[2] = 512;
-  mins[3] = 1024;
-  mins[4] = 2056;
-  mins[5] = 4112;
-  mins[6] = 8224;
-  mins[7] = 16448;
-  mins[8] = 35000;
-  mins[9] = 70000;
-  mins[10] = 140000;
-  mins[11] = 300000;
-  mins[12] = 700000;
-  mins[13] = 1800000;
-  mins[14] = 4000000;
-  mins[15] = 12500000;
-
-  table_size_tree_.setup(sizes, mins, 16);
-}
+const int table_utils::SCATTER_ARRAY_NOT_SET = -1;
+int table_utils::scatter_size_ = table_utils::SCATTER_ARRAY_NOT_SET;
 
 void table_utils::set_scatter_array(int size) {
   scatter_size_ = size;

commit bccebcd71e58aec22b029398a61592dcb2fa449f

Apply a patch for parallel runs broken in b6fb2c99
[ci skip]

include/data_structures/map_to_pos_int.hpp
@@ -17,9 +17,9 @@ class map_to_pos_int {
   void destroy();
   void recover();
 
-  bool insert(int key, int val);
+  int insert(int key, int val);
   void clear();
-  bool insert_if_empty(int key, int val, int &already_there);
+  int insert_if_empty(int key, int val);
 
   int get_careful(int key);
   int get(int key);

src/ParaRefiner.cpp
@@ -389,9 +389,9 @@ void ParaRefiner::loadHyperGraph(const ParaHypergraph &h, MPI_Comm comm) {
       assert(ij >= 0 && ij < totalVertices);
 #endif
       if (ij < minVertexIndex || ij >= maxVertexIndex) {
-        bool not_empty = toNonLocVerts.insert_if_empty(ij, numNonLocVerts, nonLocIndex);
+        nonLocIndex = toNonLocVerts.insert_if_empty(ij, numNonLocVerts);
 
-        if (not_empty) {
+        if (nonLocIndex == -1) {
           vDegs.assign(numNonLocVerts, 1);
           nonLocVerts.assign(numNonLocVerts++, ij);
         } else {

src/data_structures/map_to_pos_int.cpp
@@ -69,7 +69,7 @@ void map_to_pos_int::recover() {
   }
 }
 
-bool map_to_pos_int::insert(int key, int val) {
+int map_to_pos_int::insert(int key, int val) {
   #ifdef DEBUG_TABLES
   assert(key >= 0);
   assert(val >= 0);
@@ -114,7 +114,7 @@ bool map_to_pos_int::insert(int key, int val) {
   }
 }
 
-bool map_to_pos_int::insert_if_empty(int key, int val, int &already_there) {
+int map_to_pos_int::insert_if_empty(int key, int val) {
   #ifdef DEBUG_TABLES
   assert(key >= 0);
   #endif
@@ -131,10 +131,9 @@ bool map_to_pos_int::insert_if_empty(int key, int val, int &already_there) {
       table[slot] = val;
       keys[slot] = independent_key;
       entries.assign(size_++, slot);
-      return false;
+      return -1;
     } else {
-      already_there = table[slot];
-      return true;
+      return table[slot];
     }
   } else {
     #ifdef DEBUG_TABLES
@@ -144,10 +143,9 @@ bool map_to_pos_int::insert_if_empty(int key, int val, int &already_there) {
     if (table[key] == -1) {
       table[key] = val;
       entries.assign(size_++, key);
-      return false;
+      return -1;
     } else {
-      already_there = table[key];
-      return true;
+      return table[key];
     }
   }
 }